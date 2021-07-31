17e1a042d90a64988d60ef49ae35f5a7a1bf78d5
Dennis <64533500+rexon14@users.noreply.github.com>
2021-07-31 17:10:00 +0700

    Delete Tip Calculator directory

--- Tip Calculator/tip_calculator.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the tip calculator.")
-
-bill = float(input("What was the total bill? $"))
-ptip = float(input("What percentage tip would you like to give? "))
-nump = int(input("How many people to split the bill? "))
-
-print(f"Each person should pay: ${round(bill*(1 + ptip/100)/nump,2)}")
